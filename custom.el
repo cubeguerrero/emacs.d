5fa6e54f108c848ad468c0a996c117d3954272c0
Cuthbert Guerrero <cguerrero@zendesk.com>
2018-07-27 14:15:20 +0800

    Add YAML mode

---
+++ custom.el
@@ -0,0 +1,17 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" default)))
+ '(package-selected-packages
+   (quote
+    (yaml-mode which-key web-mode use-package smartparens smart-mode-line robe pug-mode pipenv org-download multi-term markdown-mode magit json-mode js2-mode haskell-mode flycheck exec-path-from-shell enh-ruby-mode doom-themes dockerfile-mode diminish counsel-projectile company-tern column-enforce-mode alchemist ace-window))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
