fd378565b9a13f106c57a16d16d4831c5466572c
Cuthbert Allan Y. Guerrero <cuthbert.guerrero@gmail.com>
2018-07-29 20:32:45 +0800

    Update multiple commands

--- custom.el
+++ custom.el
@@ -6,9 +6,10 @@
  '(custom-safe-themes
    (quote
     ("c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" default)))
+ '(dimmer-mode t nil (dimmer))
  '(package-selected-packages
    (quote
-    (yaml-mode which-key web-mode use-package smartparens smart-mode-line robe pug-mode pipenv org-download multi-term markdown-mode magit json-mode js2-mode haskell-mode flycheck exec-path-from-shell enh-ruby-mode doom-themes dockerfile-mode diminish counsel-projectile company-tern column-enforce-mode alchemist ace-window))))
+    (rainbow-delimiters rainbow-delimters dimmer expand-region yaml-mode which-key web-mode use-package smartparens smart-mode-line robe pug-mode pipenv org-download multi-term markdown-mode magit json-mode js2-mode haskell-mode flycheck exec-path-from-shell enh-ruby-mode doom-themes dockerfile-mode diminish counsel-projectile company-tern column-enforce-mode alchemist ace-window))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
