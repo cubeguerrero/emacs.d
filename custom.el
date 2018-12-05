571524bb017efb6266502a2540b96f562a14dbb1
Cuthbert Guerrero <cguerrero@zendesk.com>
2018-12-05 18:02:39 +0800

    Update projectile

--- custom.el
+++ custom.el
@@ -5,10 +5,10 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "2c88b703cbe7ce802bf6f0bffe3edbb8d9ec68fc7557089d4eaa1e29f7529fe1" "93a0885d5f46d2aeac12bf6be1754faa7d5e28b27926b8aa812840fe7d0b7983" "3a3de615f80a0e8706208f0a71bbcc7cc3816988f971b6d237223b6731f91605" "c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" default)))
+    ("1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" "b4c13d25b1f9f66eb769e05889ee000f89d64b089f96851b6da643cee4fdab08" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "2c88b703cbe7ce802bf6f0bffe3edbb8d9ec68fc7557089d4eaa1e29f7529fe1" "93a0885d5f46d2aeac12bf6be1754faa7d5e28b27926b8aa812840fe7d0b7983" "3a3de615f80a0e8706208f0a71bbcc7cc3816988f971b6d237223b6731f91605" "c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" default)))
  '(package-selected-packages
    (quote
-    (flucui-themes yaml-mode which-key web-mode use-package smartparens smart-mode-line robe rainbow-delimiters pug-mode pipenv org-download multi-term markdown-mode magit json-mode js2-mode haskell-mode flycheck expand-region exec-path-from-shell enh-ruby-mode doom-themes dockerfile-mode dimmer diminish counsel-projectile company-tern column-enforce-mode alchemist ace-window))))
+    (projectile counsel ivy flucui-themes yaml-mode which-key web-mode use-package smartparens smart-mode-line robe rainbow-delimiters pug-mode pipenv org-download multi-term markdown-mode magit json-mode js2-mode haskell-mode flycheck expand-region exec-path-from-shell enh-ruby-mode doom-themes dockerfile-mode dimmer diminish counsel-projectile company-tern column-enforce-mode alchemist ace-window))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
