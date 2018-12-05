d49d094bd1fc29337fc6f0384e182c8430ec20cd
Cuthbert Guerrero <cguerrero@zendesk.com>
2018-12-05 17:22:53 +0800

    Fix emacs config

--- custom.el
+++ custom.el
@@ -5,7 +5,10 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("2c88b703cbe7ce802bf6f0bffe3edbb8d9ec68fc7557089d4eaa1e29f7529fe1" "93a0885d5f46d2aeac12bf6be1754faa7d5e28b27926b8aa812840fe7d0b7983" "3a3de615f80a0e8706208f0a71bbcc7cc3816988f971b6d237223b6731f91605" "c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" default))))
+    ("a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "2c88b703cbe7ce802bf6f0bffe3edbb8d9ec68fc7557089d4eaa1e29f7529fe1" "93a0885d5f46d2aeac12bf6be1754faa7d5e28b27926b8aa812840fe7d0b7983" "3a3de615f80a0e8706208f0a71bbcc7cc3816988f971b6d237223b6731f91605" "c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" default)))
+ '(package-selected-packages
+   (quote
+    (flucui-themes yaml-mode which-key web-mode use-package smartparens smart-mode-line robe rainbow-delimiters pug-mode pipenv org-download multi-term markdown-mode magit json-mode js2-mode haskell-mode flycheck expand-region exec-path-from-shell enh-ruby-mode doom-themes dockerfile-mode dimmer diminish counsel-projectile company-tern column-enforce-mode alchemist ace-window))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
