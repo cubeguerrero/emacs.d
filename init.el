c217c0f6efc30ce9cd653144df07f9411d207233
Cuthbert Guerrero <cguerrero@zendesk.com>
2018-12-20 18:15:46 +0800

    Rename configuration.org to README.org

--- init.el
+++ init.el
@@ -7,15 +7,7 @@
 (setq custom-file "~/.emacs.d/custom.el")
 (load custom-file)
 
-(require 'package)
-(setq package-enable-at-startup nil)
-(add-to-list 'package-archives
-             '("melpa" . "http://melpa.org/packages/")
-             '("org" . "http://orgmode.org/elpa/"))
-
-(package-initialize)
-
-(org-babel-load-file "~/.emacs.d/configuration.org")
+(org-babel-load-file "~/.emacs.d/README.org")
 
 (if (file-exists-p "~/.emacs.d/configuration.local.org")
     (org-babel-load-file "~/.emacs.d/configuration.local.org"))
