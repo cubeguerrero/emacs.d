9ebd77c7f29ed403e910cc65290d2add47b0bd66
Cuthbert Guerrero <cuthbert.guerrero@gmail.com>
2019-01-11 08:53:02 +0800

    Stuff

---
+++ custom.el
@@ -0,0 +1,17 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" default)))
+ '(package-selected-packages
+   (quote
+    (zenburn-theme yaml-mode which-key web-mode use-package smartparens robe rainbow-delimiters racer pug-mode prettier-js pipenv org-download org-bullets multi-term markdown-mode magit json-mode js2-mode haskell-mode flycheck-rust expand-region exec-path-from-shell enh-ruby-mode doom-modeline doneburn-theme dockerfile-mode dimmer diminish dashboard counsel-projectile company-tern color-theme-sanityinc-tomorrow auto-dim-other-buffers all-the-icons-ivy alchemist ace-window))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
