48da1a16772452f0dcafb23e8e2c7fc0becd158d
Cuthbert Guerrero <cuthbert.guerrero@gmail.com>
2019-01-14 19:58:37 +0800

    comment-dwim-2

--- custom.el
+++ custom.el
@@ -8,7 +8,7 @@
     ("82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" default)))
  '(package-selected-packages
    (quote
-    (zenburn-theme yaml-mode which-key web-mode use-package smartparens robe rainbow-delimiters racer pug-mode prettier-js pipenv org-download org-bullets multi-term markdown-mode magit json-mode js2-mode haskell-mode flycheck-rust expand-region exec-path-from-shell enh-ruby-mode doom-modeline doneburn-theme dockerfile-mode dimmer diminish dashboard counsel-projectile company-tern color-theme-sanityinc-tomorrow auto-dim-other-buffers all-the-icons-ivy alchemist ace-window))))
+    (pomidor comment-dwim-2 zenburn-theme yaml-mode which-key web-mode use-package smartparens robe rainbow-delimiters racer pug-mode prettier-js pipenv org-download org-bullets multi-term markdown-mode magit json-mode js2-mode haskell-mode flycheck-rust expand-region exec-path-from-shell enh-ruby-mode doom-modeline doneburn-theme dockerfile-mode dimmer diminish dashboard counsel-projectile company-tern color-theme-sanityinc-tomorrow auto-dim-other-buffers all-the-icons-ivy alchemist ace-window))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
