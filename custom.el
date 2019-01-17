5daf2060832809e9aa72ce7110b5113fb9439aff
Cuthbert Guerrero <cguerrero@zendesk.com>
2019-01-17 10:36:45 +0800

    Ag.el

---
+++ custom.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (ag yaml-mode which-key web-mode use-package smartparens slim-mode robe rainbow-delimiters racer pug-mode prettier-js pipenv org-download org-bullets multi-term markdown-mode magit json-mode js2-mode haskell-mode flycheck-rust expand-region exec-path-from-shell enh-ruby-mode doom-themes doom-modeline dockerfile-mode dimmer diminish dashboard counsel-projectile company-tern comment-dwim-2 color-theme-sanityinc-tomorrow auto-dim-other-buffers all-the-icons-ivy alchemist ace-window))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 2.0)))))
