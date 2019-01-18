dfb12e0e12ecc54846adc9dc57e5ded9bac90670
Cuthbert Guerrero <cguerrero@zendesk.com>
2019-01-18 13:31:04 +0800

    Doom Dracula

--- custom.el
+++ custom.el
@@ -3,6 +3,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("6b289bab28a7e511f9c54496be647dc60f5bd8f9917c9495978762b99d8c96a0" "b54826e5d9978d59f9e0a169bbd4739dd927eead3ef65f56786621b53c031a7c" "6b2636879127bf6124ce541b1b2824800afc49c6ccd65439d6eb987dbf200c36" default)))
  '(package-selected-packages
    (quote
     (ag yaml-mode which-key web-mode use-package smartparens slim-mode robe rainbow-delimiters racer pug-mode prettier-js pipenv org-download org-bullets multi-term markdown-mode magit json-mode js2-mode haskell-mode flycheck-rust expand-region exec-path-from-shell enh-ruby-mode doom-themes doom-modeline dockerfile-mode dimmer diminish dashboard counsel-projectile company-tern comment-dwim-2 color-theme-sanityinc-tomorrow auto-dim-other-buffers all-the-icons-ivy alchemist ace-window))))
