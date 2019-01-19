af61aafaed74f9ac6ab0a7feec6ca8ec5d300dcd
Cuthbert Guerrero <cuthbert.guerrero@gmail.com>
2019-01-19 13:41:22 +0800

    Something

--- custom.el
+++ custom.el
@@ -5,10 +5,10 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("6b289bab28a7e511f9c54496be647dc60f5bd8f9917c9495978762b99d8c96a0" "b54826e5d9978d59f9e0a169bbd4739dd927eead3ef65f56786621b53c031a7c" "6b2636879127bf6124ce541b1b2824800afc49c6ccd65439d6eb987dbf200c36" default)))
+    ("8aca557e9a17174d8f847fb02870cb2bb67f3b6e808e46c0e54a44e3e18e1020" "151bde695af0b0e69c3846500f58d9a0ca8cb2d447da68d7fbf4154dcf818ebc" "75d3dde259ce79660bac8e9e237b55674b910b470f313cdf4b019230d01a982a" "d1b4990bd599f5e2186c3f75769a2c5334063e9e541e37514942c27975700370" "100e7c5956d7bb3fd0eebff57fde6de8f3b9fafa056a2519f169f85199cc1c96" "a3fa4abaf08cc169b61dea8f6df1bbe4123ec1d2afeb01c17e11fdc31fc66379" "7e78a1030293619094ea6ae80a7579a562068087080e01c2b8b503b27900165c" "6b289bab28a7e511f9c54496be647dc60f5bd8f9917c9495978762b99d8c96a0" "b54826e5d9978d59f9e0a169bbd4739dd927eead3ef65f56786621b53c031a7c" "6b2636879127bf6124ce541b1b2824800afc49c6ccd65439d6eb987dbf200c36" default)))
  '(package-selected-packages
    (quote
-    (ag yaml-mode which-key web-mode use-package smartparens slim-mode robe rainbow-delimiters racer pug-mode prettier-js pipenv org-download org-bullets multi-term markdown-mode magit json-mode js2-mode haskell-mode flycheck-rust expand-region exec-path-from-shell enh-ruby-mode doom-themes doom-modeline dockerfile-mode dimmer diminish dashboard counsel-projectile company-tern comment-dwim-2 color-theme-sanityinc-tomorrow auto-dim-other-buffers all-the-icons-ivy alchemist ace-window))))
+    (company-ghc ag yaml-mode which-key web-mode use-package smartparens slim-mode robe rainbow-delimiters racer pug-mode prettier-js pipenv org-download org-bullets multi-term markdown-mode magit json-mode js2-mode haskell-mode flycheck-rust expand-region exec-path-from-shell enh-ruby-mode doom-themes doom-modeline dockerfile-mode dimmer diminish dashboard counsel-projectile company-tern comment-dwim-2 color-theme-sanityinc-tomorrow auto-dim-other-buffers all-the-icons-ivy alchemist ace-window))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
