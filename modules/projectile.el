e0d2f092c2268456cd45f3ace78ba2f45b457e3f
Cuthbert Guerrero <cuthbert.guerrero@gmail.com>
2019-03-06 23:03:31 +0800

    Use ivy

--- modules/projectile.el
+++ modules/projectile.el
@@ -8,10 +8,17 @@
   :config
   (add-to-list 'projectile-globally-ignored-directories "*node_modules")
   (projectile-mode +1)
-  (define-key projectile-mode-map (kbd "M-p") 'projectile-command-map)
+  (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
   (setq projectile-mode-line
         '(:eval (format " [%s]" (projectile-project-name))))
-  (setq projectile-remember-window-configs t))
+  (setq projectile-remember-window-configs t
+        projectile-completion-system 'ivy))
+
+(use-package counsel-projectile
+  :ensure t
+  :diminish (counsel-projectile-mode)
+  :config
+  (counsel-projectile-mode 1))
 
 (provide 'projectile)
 ;;; projectile.el ends here
