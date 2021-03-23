9024d9c2b107a812ec85e9ef7bd034ab77bbb33f
Cuthbert Guerrero <cuthbert.guerrero@gmail.com>
2021-03-23 11:38:25 +0800

    Update to ido, enh-ruby-mode

--- custom.el
+++ custom.el
@@ -6,7 +6,7 @@
  '(custom-safe-themes
    '("7546a14373f1f2da6896830e7a73674ef274b3da313f8a2c4a79842e8a93953e" "e27556a94bd02099248b888555a6458d897e8a7919fd64278d1f1e8784448941" "3df5335c36b40e417fec0392532c1b82b79114a05d5ade62cfe3de63a59bc5c6" "f94110b35f558e4c015b2c680f150bf8a19799d775f8352c957d9d1054b0a210" default))
  '(package-selected-packages
-   '(lsp-mode doom-themes which-key lsp-ui ag magit projectile diminish go-mode doom-modeline ace-window use-package)))
+   '(enh-ruby-mode visual-fill-column org-bullets lsp-mode doom-themes which-key lsp-ui ag magit projectile diminish go-mode doom-modeline ace-window use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- init.el
+++ init.el
@@ -47,6 +47,11 @@
 (global-set-key (kbd "C-M-r") 'isearch-backward)
 
 (set-face-attribute 'default nil :font "Fira Code Retina" :height 120)
+;; Set the fixed pitch face
+(set-face-attribute 'fixed-pitch nil :font "Fira Code Retina" :height 120)
+
+;; Set the variable pitch face
+(set-face-attribute 'variable-pitch nil :font "Helvetica" :height 120 :weight 'regular)
 
 (load-theme 'leuven)
 
@@ -68,11 +73,17 @@
 (setq use-package-always-ensure t)
 
 ;; Packages
-;;; Ido
+;;; ido
 (setq ido-enable-flex-matching t
-      ido-everywhere t
-      ido-create-new-buffer 'always)
+     ido-everywhere t
+     ido-use-faces nil
+     ido-create-new-buffer 'always)
 (ido-mode 1)
+(ido-everywhere 1)
+
+(use-package flx-ido
+  :config
+  (flx-ido-mode t))
 
 ;;; Diminish
 (use-package diminish
@@ -102,6 +113,11 @@
   (use-package exec-path-from-shell
     :init (exec-path-from-shell-initialize)))
 
+;;; Whichkey
+(use-package which-key
+  :config
+  (setq which-key-idle-delay 0.3))
+
 ;;; Projectile
 (use-package projectile
   :diminish (projectile-mode)
@@ -116,6 +132,7 @@
   (when (file-directory-p "~/Code")
     (setq projectile-project-search-path '("~/Code")))
   (setq projectile-remember-window-configs t
+        projectile-switch-project-action #'projectile-dired
         projectile-completion-system 'ido))
 
 ;;; Ag
@@ -142,6 +159,58 @@
 (use-package go-mode
   :mode "\\.go\\'"
   :hook (go-mode . lsp-deferred))
+
+;;; Ruby
+(use-package enh-ruby-mode
+  :mode "\\(?:\\.rb\\|ru\\|rake\\|thor\\|jbuilder\\|gemspec\\|podspec\\|/\\(?:Gem\\|Rake\\|Cap\\|Thor\\|Vagrant\\|Guard\\|Pod\\)file\\)\\'"
+  :hook (enh-ruby-mode . lsp-deferred))
+
+;;; Org
+(defun cubeg/org-mode-setup ()
+  (org-indent-mode)
+  (variable-pitch-mode 1)
+  (visual-line-mode 1))
+
+(defun cubeg/org-font-setup ()
+  ;; Set faces for heading levels
+  (dolist (face '((org-level-1 . 1.2)
+                  (org-level-2 . 1.1)
+                  (org-level-3 . 1.05)
+                  (org-level-4 . 1.0)
+                  (org-level-5 . 1.1)
+                  (org-level-6 . 1.1)
+                  (org-level-7 . 1.1)
+                  (org-level-8 . 1.1)))
+    (set-face-attribute (car face) nil :font "Helvetica" :weight 'regular :height (cdr face)))
+
+  ;; Ensure that anything that should be fixed-pitch in Org files appears that way
+  (set-face-attribute 'org-block nil :foreground nil :inherit 'fixed-pitch)
+  (set-face-attribute 'org-code nil   :inherit '(shadow fixed-pitch))
+  (set-face-attribute 'org-table nil   :inherit '(shadow fixed-pitch))
+  (set-face-attribute 'org-verbatim nil :inherit '(shadow fixed-pitch))
+  (set-face-attribute 'org-special-keyword nil :inherit '(font-lock-comment-face fixed-pitch))
+  (set-face-attribute 'org-meta-line nil :inherit '(font-lock-comment-face fixed-pitch))
+  (set-face-attribute 'org-checkbox nil :inherit 'fixed-pitch))
+
+(use-package org
+  :hook (org-mode . cubeg/org-mode-setup)
+  :config
+  (setq org-ellipsis " ▾")
+  (cubeg/org-font-setup))
+
+(use-package org-bullets
+  :after org
+  :hook (org-mode . org-bullets-mode)
+  :custom
+  (org-bullets-bullet-list '("◉" "○" "●" "○" "●" "○" "●")))
+
+(defun cubeg/org-mode-visual-fill ()
+  (setq visual-fill-column-width 100
+        visual-fill-column-center-text t)
+  (visual-fill-column-mode 1))
+
+(use-package visual-fill-column
+  :hook (org-mode . cubeg/org-mode-visual-fill))
 
 ;; Write other customization above this
 ;; Do not litter the init file, all settings done in
