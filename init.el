aabb158daee6de0c54c9d23dc9f6c99ba3a8711b
Cuthbert Guerrero <cuthbert.guerrero@gmail.com>
2021-03-27 20:17:43 +0800

    fix errors

--- init.el
+++ init.el
@@ -5,8 +5,7 @@
 
 (add-to-list 'package-archives
              '("melpa" . "https://melpa.org/packages/")
-             '("org" . "https://orgmode.org/elpa/")
-             '("elpa" . "https://elpa.gnu.org/packages/"))
+             '("org" . "https://orgmode.org/elpa/"))
 
 (package-initialize)
 (unless package-archive-contents
@@ -80,7 +79,7 @@
 (diminish 'auto-revert-mode)
 (diminish 'visual-line-mode)
 (diminish 'eldoc-mode)
-(diminish 'abbrev-mode
+(diminish 'abbrev-mode)
 
 (use-package doom-themes
   :config
